b61f2d956a9e87af0ead07b572d30d4e8afa1cbc
dada542 <45128986+dada542@users.noreply.github.com>
2021-01-03 22:01:40 +0100

    Update controller.js

--- game-engine/controller.js
+++ game-engine/controller.js
@@ -1,1 +1,19 @@
+const Controller = function() {
 
+this.keyUpDown = function(event) {
+
+var down = (event.type == "keydown") ? true : false;
+
+alert(event.key);
+
+}
+
+this.handleKeyUpDown = (event) => { this.keyUpDown(event); }
+
+}
+
+Controller.prototype = {
+
+constructor: Controller
+
+}
